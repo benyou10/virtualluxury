eaef74d80559cc5efaf0ef5d09c4e791929ca563
benyou10 <ybenzian016@gmail.com>
2024-09-19 21:35:57 +0100

    fix3

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,39 +3,30 @@ import path from 'path';
 import matter from 'gray-matter';
 import Link from 'next/link';
 
-export default function Home() {
-  let posts = [];
-  const postsDirectory = path.join(process.cwd(), 'posts');
+const postsDirectory = path.join(process.cwd(), 'posts');
 
-  try {
-    const filenames = fs.readdirSync(postsDirectory);
-    posts = filenames.map((filename) => {
-      const filePath = path.join(postsDirectory, filename);
-      const fileContents = fs.readFileSync(filePath, 'utf8');
-      const { data } = matter(fileContents);
-      return {
-        slug: filename.replace('.md', ''),
-        title: data.title,
-      };
-    });
-  } catch (error) {
-    console.error('Error reading posts directory:', error.message);
-  }
+export default function Home() {
+  const filenames = fs.readdirSync(postsDirectory);
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const { data } = matter(fileContents);
+    return {
+      slug: filename.replace('.md', ''),
+      title: data.title,
+    };
+  });
 
   return (
     <main>
       <h1 className='text-black text-6xl'>All Blog Posts</h1>
-      {posts.length === 0 ? (
-        <p>No blog posts available.</p>
-      ) : (
-        <ul>
-          {posts.map((post) => (
-            <li key={post.slug}>
-              <Link href={`/blogs/${post.slug}`}>{post.title}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/blogs/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
